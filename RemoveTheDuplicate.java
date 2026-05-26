5d568f9d2bfc3403871962885c9ba273f8999c05
Bharath <bharath23695a0508@gmail.com>
2026-05-26 16:51:36 +0530

    Remove the Duplicate Using Contains Method

---
+++ RemoveTheDuplicate.java
@@ -0,0 +1,15 @@
+public class RemoveTheDuplicate {
+    public static void main(String[] args) {
+        String s1="javadev";
+        String s2="";
+        for(int i=0;i<s1.length();i++)
+        {
+            char ch=s1.charAt(i);
+            if(!s2.contains(ch+""))
+            {
+                s2+=ch;
+            }
+        }
+        System.out.println(s2);
+    }
+}
