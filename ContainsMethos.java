6e2daf501235b3f2a7724034b76f562777da8203
Bharath <bharath23695a0508@gmail.com>
2026-05-26 16:48:18 +0530

    ContainsBasic Program

---
+++ ContainsMethos.java
@@ -0,0 +1,9 @@
+public class ContainsMethos {
+    public static void main(String[] args) {
+        String s1="javadev";
+        System.out.println(s1.contains("j"));//true
+        System.out.println(s1.contains("ava"));//true
+        System.out.println(s1.contains("dev"));//true
+        System.out.println(s1.contains("Java"));//false
+    }
+}
